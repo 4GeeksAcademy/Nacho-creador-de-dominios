baee694237aee37031f006c93a0a385f7f6710b9
Nacho <ignaciovelamd@gmail.com>
2025-11-17 10:14:29 +0000

    generadorDominios

--- src/app.js
+++ src/app.js
@@ -9,3 +9,27 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+function domainGenerator() {
+    let pronoun = ["the", "our"];
+    let adj = ["great", "big"];
+    let noun = ["jogger", "racoon"];
+    let domainEnd = [".com", ".es"];
+
+    let domainNames = [];
+    for (let p = 0; p < pronoun.length; p++) {
+      for (let a = 0; a < adj.length; a++) {
+        for (let n = 0; n < noun.length; n++) {
+          for (let d = 0; d < domainEnd.length; d++) {
+            let domainName =
+            pronoun[p] + adj[a] + noun[n] + domainEnd[d];
+            domainNames.push(domainName);
+          }
+        }
+      }
+    }
+    console.log(domainNames);
+    return domainNames;
+  }
+
+  domainGenerator()
